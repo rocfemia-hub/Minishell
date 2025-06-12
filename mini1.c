8f1b382a0e12b7666cf3e715faf2a1549caeb3fa
Your Name <you@example.com>
2025-06-12 18:00:33 +0200

    añadiendo el comienzo de echo

--- mini1.c
+++ mini1.c
@@ -6,7 +6,7 @@
 /*   By: roo <roo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 20:56:08 by roo               #+#    #+#             */
-/*   Updated: 2025/06/12 14:30:29 by roo              ###   ########.fr       */
+/*   Updated: 2025/06/12 17:55:51 by roo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 		return 1;
 	while(1)
 	{
-		line = readline("minishell>");
+		line = readline("minishell> ");
 		if(!line)
 			break;
 		vars->params = ft_split(line, ' ');
@@ -36,8 +36,20 @@ int main(int argc, char **argv)
 
 void commands_control(t_vars *vars)
 {
-	if(vars->params[0] && ft_strncmp(vars->params[0], "echo", 5) == 0)
+	if(vars->params[0] && ft_strncmp(vars->params[0], "echo", 5) == 0) //cambiar todo para que salga por file descriptor
 	{
-		printf("patata");
+		int i;
+
+		i = 1;
+		while(vars->params[i])
+		{
+			printf("%s", vars->params[i]);
+			if(vars->params[i + 1])
+				printf(" ");
+			i++;
+		}
+		printf("\n");
 	}
+	if(vars->params[0] && ft_strncmp(vars->params[0], "pwd", 4) == 0)
+	{}
 }
