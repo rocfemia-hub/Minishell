17d787fa70422234323bcfff20a4b31d68bfb9fc
Your Name <you@example.com>
2025-07-07 23:21:09 +0200

    modificando commands1.c y commands2.c y haciendo funcionar los built-in

--- parser/commands1.c
+++ parser/commands1.c
@@ -1,7 +1,7 @@
 #include "../minishell.h"
 
 void echo_com(t_com *temp, char *line)
-{ // echo_com implementada (ro)
+{ // MODIFICADA
 	// printf("ha entrado a echo\n");
 	int i;
 	int j;
@@ -35,7 +35,7 @@ void echo_com(t_com *temp, char *line)
 }
 
 void pwd_com(t_com *temp, char *line)
-{  // pwd_com modificada (ro)
+{ // MODIFICADA
     // printf("ha entrado a pwd\n");
     int i = 0;
 
@@ -46,7 +46,7 @@ void pwd_com(t_com *temp, char *line)
         temp->command = ft_strdup("pwd");
         temp->flag_built = 1;
         temp->command_arg = ft_strdup(line + i);
-        temp->arg = ft_strdup(""); //ft_strdup(line + i + 4);
+        temp->arg = ft_strdup("");
     }
 	else // inicializamos con valores seguros si no coincide
     {
@@ -58,7 +58,7 @@ void pwd_com(t_com *temp, char *line)
 }
 
 void cd_com(t_com *temp, char *line)
-{ // cd_com modificada (ro)
+{ // MODIFICADA
     // printf("ha entrado a cd\n");
     int i;
 	int j;
@@ -92,33 +92,47 @@ void cd_com(t_com *temp, char *line)
 }
 
 void export_com(t_com *temp, char *line)
-{ // gestiona el export para meter cada cosa en la estructura
+{ // MODIFICADA
     // printf("ha entrado a export\n");
     int i = 0;
 
-    while (line[i] == 32)
+    while (line[i] == 32) // para saltar los espacios al principio
         i++;
-    if (line[i] && line[i] == 'e' && line[i + 1] == 'x' && line[i + 2] == 'p' && line[i + 3] == 'o' && line[i + 4] == 'r' && line[i + 5] == 't' && line[i + 6] == ' ')
+    if (ft_strncmp(line + i, "export", 6) == 0 && (line[i + 6] == ' ' || line[i + 6] == '\0')) // debe haber espacio o ser el final de la string
     {
         temp->command = ft_strdup("export");
         temp->flag_built = 1;
         temp->command_arg = ft_strdup(line + i);
-        temp->arg = ft_strdup(line + i + 7);
+        temp->arg = ft_strdup("");
+    }
+	else // inicializamos con valores seguros si no coincide
+    {
+        temp->command = ft_strdup("unknown");
+        temp->command_arg = ft_strdup(line);
+        temp->arg = ft_strdup("");
+        temp->flag_built = 0;
     }
 }
 
 void unset_com(t_com *temp, char *line)
-{ // gestiona el unset para meter cada cosa en la estructura
+{ // MODIFICADA
     // printf("ha entrado a unset\n");
     int i = 0;
 
-    while (line[i] == 32)
+    while (line[i] == 32) // para saltar los espacios al principio
         i++;
-    if (line[i] && line[i] == 'u' && line[i + 1] == 'n' && line[i + 2] == 's' && line[i + 3] == 'e' && line[i + 4] == 't' && line[i + 5] == ' ')
+    if (ft_strncmp(line + i, "unset", 5) == 0 && (line[i + 5] == ' ' || line[i + 5] == '\0')) // debe haber espacio o ser el final de la string
     {
         temp->command = ft_strdup("unset");
         temp->flag_built = 1;
         temp->command_arg = ft_strdup(line + i);
-        temp->arg = ft_strdup(line + i + 6);
+        temp->arg = ft_strdup("");
+    }
+	else // inicializamos con valores seguros si no coincide
+    {
+        temp->command = ft_strdup("unknown");
+        temp->command_arg = ft_strdup(line);
+        temp->arg = ft_strdup("");
+        temp->flag_built = 0;
     }
 }
