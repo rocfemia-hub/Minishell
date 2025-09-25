9851985766f5b9437076f6f7eb92fd253d8d5b73
Your Name <you@example.com>
2025-09-25 18:49:46 +0200

    cambiando builting d env

--- exec/built_ins2.c
+++ exec/built_ins2.c
@@ -6,7 +6,7 @@
 /*   By: roo <roo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 01:18:22 by roo               #+#    #+#             */
-/*   Updated: 2025/09/08 20:41:28 by roo              ###   ########.fr       */
+/*   Updated: 2025/09/25 18:45:58 by roo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 void env_function(t_com *list, t_vars *vars)
 { // Ya funciona correctamente :)))
-	int i = 0;
+	t_env	*env_list;
+	char	*line;
 
 	if (!vars->env)
 		return;
-	while (vars->env[i])
+	while (env_list)
 	{
-		write(list->fd_out, vars->env[i], ft_strlen(vars->env[i])); // simplemente imprime linea a linea lo que hay en el env, sin opciones ni argmentos (lo pone en el subject)
-		write(list->fd_out, "\n", 1);
-		i++;
+		line = ft_strjoin(env_list->env_name, "=");
+        line = ft_strjoin_gnl(line, env_list->env_inf);
+		write(list->fd_out, line, ft_strlen(line));
+        write(list->fd_out, "\n", 1);
+		free(line);
+        env_list = env_list->next;
 	}
 }
 
